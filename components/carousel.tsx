c41ef82caddc579c38efe2610c63df44355fdbc7
luigi-bruni <luigi_bruni@hotmail.it>
2023-04-08 17:31:32 +0200

    upd embla

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { useEmblaCarousel } from 'embla-carousel/react'
+import useEmblaCarousel from 'embla-carousel-react'
 import styles from './Carousel.module.css'
 import Image from 'next/image'
 import { useCallback } from 'react'
